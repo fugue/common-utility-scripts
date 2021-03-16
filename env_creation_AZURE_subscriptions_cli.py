4ca427f52457fe78fda96d95e15cde8a70e7fc18
Ankush Khurana <ankush@fugue.co>
2021-03-16 12:50:01 -0400

    Update env_creation_AZURE_subscriptions_cli.py
    Add support to turn on/off duplicate environment creation using  'allow_dups' flag.

--- env_creation_AZURE_subscriptions_cli.py
+++ env_creation_AZURE_subscriptions_cli.py
@@ -10,20 +10,24 @@ import getpass
 # interval: scan interval in seconds. Default is 24hrs 
 # compliance_families: List of complaince families needed https://docs.fugue.co/api.html#api-compliance-format
 # subscriptions: map of Azure Application Name, credentials and Resource Groups that need to be loaded into Fugue.
-# Format "App Name": ["Tenant Id", "Subscription Id", "Application ID", [Resource Groups]].
-# Client Secret will be prompted at command prompt
+# Format "App Name": ["Tenant Id", "Subscription Id", "Application ID", "Client Secret", [Resource Groups]].
+# Client Secret can be left blank or set to "-" if the CLI opted value needs to be used
 # Default for Resource Group value is "*" for automatically discovering and adding all resource groups. 
 # For selective resource groups, use the format ["example-rg","another-rg"] 
 # Environments are created with the App name. Details on how to create these: https://docs.fugue.co/setupazure.html#step-2a-connect-to-azure
+# allow_dups: Default = False. Flag to allow duplicate environment creation in Fugue. 
+    # If set to False, a list of existing environment will be retrieved from Fugue and only applications not in Fugue will be created.  
 
 provider = "azure"
 interval = "86400"
 compliance_families = ["CISAZURE"]
+allow_dups = False
 subscriptions = {
     "Prod App": ["tenant id", "subscription id", "app id", ["*"]],
     "Dev App": ["2", "2", "2", ["example-rg","another-rg"]],
     "QA App": ["3", "3", "3", ["example-rg","another-rg"]]
 }
+
 
 # Fugue API base URL
 api_url = "https://api.riskmanager.fugue.co"
@@ -52,6 +56,24 @@ def get(path, params=None):
     """
     url = '%s/%s/%s' % (api_url, api_ver, path.strip('/'))
     return requests.get(url, params=params, auth=auth).json()
+
+def get_app_list(provider):
+    """
+        Get list of Azure environments in Fugue tenant and extract the Applications IDs from the credentials.   
+    """
+    params = {
+        'q.provider': provider,
+    }
+    try:
+        env_list = get('environments', params=params)
+    except Exception as error: 
+            sys.exit("Error:" + error) 
+    else: 
+        app_id_list = []
+        for env in env_list['items']:
+            app_id_list.append(env['provider_options']['azure']['application_id'])
+       
+        return app_id_list
 
 def create_env(path, json=None):
     """
@@ -104,29 +126,36 @@ def main():
             else:
                 exit("Secret was not entered.")   
         
-        for name, provider_options in subscriptions.items():
-        # Set environment name
-            env_name = name
+        # If allow_dups = False, get list of Azure envrionments from Fugue and extract the application ID from credentials
+        if allow_dups == False:
+           print ("Duplicate environments are not allowed. Retrieving list of environments and application id" + "\n") 
+           existing_app_list = get_app_list(provider)  
+           print ("Existing applications list retrieved (" + str(len(existing_app_list)) + ")" + "\n") 
 
-        # Check if key is listed in the subscription details. 
-        # If it is then use the listed key otherwise use the key from CLI 
+        for name, provider_options in subscriptions.items():
+            # Set environment name and credentials
+            env_name = name
             credentials = provider_options[0:3]
+            app_id = provider_options[2]
             credentials.append(key)
             resource_groups = provider_options[3]
-            
-            print("Starting creation for environment " + env_name)
-            # Create JSON body  
-            env_def = create_azure_env_def(env_name, provider.lower(), credentials, compliance_families, resource_groups, interval)
-            print ("JSON body created for environment " + env_name )
-                
-            #Create environment
-            resp = create_env('environments', env_def)
-                    
-            if resp.status_code != 201:
-                print('Environment creation failed for App: ' + name + ' with response code: {}'.format(resp.status_code) + ' and reason: {}'.format(resp.text) + "\n") 
+
+            if allow_dups == False and app_id in existing_app_list:   
+                print ("Found application id in existing environment list. Skipping environment creation for - " + name + ": " + app_id + "\n")
             else:
-                env_id = resp.json()['id'] 
-                print ('Environment created for App: ' + name + ' with environment name: ' + resp.json()['name'] + ' and environment id: ' + resp.json()['id'] + "\n") 
+                print("Starting creation for environment " + env_name)
+                # Create JSON body  
+                env_def = create_azure_env_def(env_name, provider.lower(), credentials, compliance_families, resource_groups, interval)
+                print ("JSON body created for environment " + env_name )
+                    
+                #Create environment
+                resp = create_env('environments', env_def)
+                        
+                if resp.status_code != 201:
+                    print('Environment creation failed for App: ' + name + ' with response code: {}'.format(resp.status_code) + ' and reason: {}'.format(resp.text) + "\n") 
+                else:
+                    env_id = resp.json()['id'] 
+                    print ('Environment created for App: ' + name + ' with environment name: ' + resp.json()['name'] + ' and environment id: ' + resp.json()['id'] + "\n") 
 
 if __name__ == '__main__':
     main()            
